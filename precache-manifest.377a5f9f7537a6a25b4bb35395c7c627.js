9a2430d22f14e1a84973b5174feaa4007197f613
Sharmin <thelordcloud@gmail.com>
2020-11-26 21:16:28 +0200

    Updates

---
+++ precache-manifest.377a5f9f7537a6a25b4bb35395c7c627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29a19340795b54c367d2ae8308b057c",
+    "url": "/cocktail-database/index.html"
+  },
+  {
+    "revision": "e017d4a222cf45175745",
+    "url": "/cocktail-database/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "01b47b64449816ff0ea6",
+    "url": "/cocktail-database/static/js/2.e8d8e3b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktail-database/static/js/2.e8d8e3b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e017d4a222cf45175745",
+    "url": "/cocktail-database/static/js/main.0d35a9ea.chunk.js"
+  },
+  {
+    "revision": "d2e8de089dd39954820d",
+    "url": "/cocktail-database/static/js/runtime-main.a636f496.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktail-database/static/media/logo.9a3d2645.svg"
+  }
+]);
